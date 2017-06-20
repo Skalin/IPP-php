b95edcc7c5d55c3f66eec91cd323c880e3b273e6
Dominik Skála <DominikSkala@seznam.cz>
2017-06-20 18:25:38 +0200

    Update syn.php

--- syn.php
+++ syn.php
@@ -18,42 +18,42 @@
 	$flags = array("if" => false, "of" => false, "ff" => false, "br" => false);
 	$files = array("inputFile" => "stdin", "outputFile" => "stdout", "formatFile" => "none");
 
-		// we will now check for arguments validity and then we'll check for integrity of all files
-		$common->checkArguments($argv);
+	// we will now check for arguments validity and then we'll check for integrity of all files
+	$common->checkArguments($argv);
 
-		// shortcuts for array access
-		$iFile = $files["inputFile"];
-		$oFile = $files["outputFile"];
-		$fFile = $files["formatFile"];
+	// shortcuts for array access
+	$iFile = $files["inputFile"];
+	$oFile = $files["outputFile"];
+	$fFile = $files["formatFile"];
 
-		// let's parse it!
-		$format = $parser->parseFormatFile($common, $fFile);
+	// let's parse it!
+	$format = $parser->parseFormatFile($common, $fFile);
 
-		// now we're deciding whether to read from stdin or from input file passed in arguments
-		if (!$flags["if"] && $files["inputFile"] == "stdin") {
-			$input = $editor->readFromStdinToInput();
-		} else {
-			$input = $editor->readFromFileToInput($common, $iFile);
-		}
-
-
-			// $input is being formatted into $output using formatting file
-			if ($flags["ff"] != false && "formatFile" != "none") {
-				$output = $editor->editInputFile($input, $format);
-			} else {
-				$output = $input;
-			}
-			// script will insert <br /> tag at end of each line, the function itself checks the tag
-			$output = $editor->insertNewLine($flags, $output);
+	// now we're deciding whether to read from stdin or from input file passed in arguments
+	if (!$flags["if"] && $files["inputFile"] == "stdin") {
+		$input = $editor->readFromStdinToInput();
+	} else {
+		$input = $editor->readFromFileToInput($common, $iFile);
+	}
 
 
-		// we will now decide whether to print to stdout or to file passed in arguments
-		if ($files["outputFile"] == "stdout" && !$flags["of"]) {
-			$editor->writeToStdout($output);
-		} else {
-			$editor->writeToFile($common, $output, $oFile);
-		}
+	// $input is being formatted into $output using formatting file
+	if ($flags["ff"] != false && "formatFile" != "none") {
+		$output = $editor->editInputFile($input, $format);
+	} else {
+		$output = $input;
+	}
+	// script will insert <br /> tag at end of each line, the function itself checks the tag
+	$output = $editor->insertNewLine($flags, $output);
 
-		return 0;
+
+	// we will now decide whether to print to stdout or to file passed in arguments
+	if ($files["outputFile"] == "stdout" && !$flags["of"]) {
+		$editor->writeToStdout($output);
+	} else {
+		$editor->writeToFile($common, $output, $oFile);
+	}
+
+	return 0;
 
 ?>
